3e947d0f93a4ca773c7fc945da15cbbe38fd13fe
Rebecca Wagaman <ribexy@gmail.com>
2017-07-31 23:48:51 +0000

    Quotes appear on page with character

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+    var url = "https://got-quotes.herokuapp.com/quotes";
     
+    $.ajax({
+        url: url,
+        success: function(response){
+            var quote = response.quote;
+            var character = response.character;
+            console.log(response);
+            var quoteHtml = "<p>" + quote + "</p>";
+            var charHtml = "<p>--- " + character + "</p>";
+            $("#quote").html(quoteHtml);
+            $("#character").html(charHtml);
+        }
+    });
+    
+    $("#quote").submit(function(event) {
+        event.preventDefault();
+        
+    })
 });
