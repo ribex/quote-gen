b9e34cda1abfdf331bacf05aac097447aac08954
Rebecca Wagaman <ribexy@gmail.com>
2017-08-10 23:02:36 +0000

    Functional twitter button, can tweet quote with character who said it

--- script.js
+++ script.js
@@ -22,9 +22,27 @@ var makeAQuote = function() {
             var charHtml = "<p>--- " + character + "</p>";
             $("#quote").html(quoteHtml);
             $("#character").html(charHtml);
-            var twitterHtml = '<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=' + quote + " --" + character + '">Tweet</a>';
-$("#tweet").html(twitterHtml);
+            var twitterHtml = '<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=' + quote + " --" + character + '&hashtags=gameofthrones&size=large">Tweet</a>';
+            $("#tweet").html(twitterHtml);
         }
     });
 };
+
+
+// window.twttr = (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0],
+//     t = window.twttr || {};
+//   if (d.getElementById(id)) return t;
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://platform.twitter.com/widgets.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+
+//   t._e = [];
+//   t.ready = function(f) {
+//     t._e.push(f);
+//   };
+
+//   return t;
+// }(document, "script", "twitter-wjs"));
 
